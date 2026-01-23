62120c435a0a986c8d505df96f32c2d457913fc8
Leonardo Riether <leonardo.riether@incognia.com>
2026-01-23 09:35:07 -0300

    ops

--- gh/gh.go
+++ gh/gh.go
@@ -42,8 +42,8 @@ func SearchPRs(authors []string, org string, createdAfter time.Time) (PullReques
 func makeSearchPRArgs(authors []string, org string, createdAfter time.Time) []string {
 	args := []string{
 		"search", "prs",
-		// "--created", ">=" + createdAfter.Format(time.DateOnly),
-		"--json", "author,createdAt,updatedAt,title,state,url",
+		"--created", ">=" + createdAfter.Format(time.DateOnly),
+		"--json", "author,createdAt,updatedAt,title,state,isDraft,url",
 		"--",
 	}
 
